3e48b8a5439948d7fe8bd41b6f51f96b0ed9dc2e
david.gmeindl <david.gmeindl@gmail.com>
2023-08-04 16:35:27 +0200

    added tests of inheritence casting

--- TestInheritence/TestInheritence.java
+++ TestInheritence/TestInheritence.java
@@ -5,8 +5,15 @@ class OptimizedDiskCache extends DiskCache {}
 
 public class TestInheritence {
     public static void main(String[] args) {
+        // upcasting
         Cache cache = new Cache();
-        MemoryCache memoryCache = (MemoryCache)cache;
+        //MemoryCache memoryCache = (MemoryCache)cache; // upcasting fails
+
+        // down and then upcasting works
+        OptimizedDiskCache optDiskCache = new OptimizedDiskCache();
+        Cache cache2 = (Cache)optDiskCache;
+        DiskCache cache3 = (DiskCache)cache2;
+        OptimizedDiskCache cache4 = (OptimizedDiskCache)cache2;
     }
 }
 
